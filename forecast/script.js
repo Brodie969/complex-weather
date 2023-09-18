899649944441eb49e73e63d6666ffb171b249551
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-09-18 22:21:08 +1000

    Finally Working Forecast

--- forecast/script.js
+++ forecast/script.js
@@ -1,49 +1,86 @@
 import { API_KEY } from "../secrets.js";
 
+const numberOfDays = 7; // Define the number of forecast days as a constant
+
 const parameters = new URLSearchParams(window.location.search);
 const cityParameter = parameters.get("city");
 
 function currentForecast(city) {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=${numberOfDays + 1}`)
         .then(response => response.json())
         .then(data => {
+            // Extract location and time
             const location = data.location.name;
             const time = data.location.localtime;
-            const day1Date = data.forecast.forecastday[1].date;
-            const day1MaxTemp = data.forecast.forecastday[1].day.maxtemp_c;
 
-            // Create HTML elements to display the data
-            const locationElement = document.createElement("p");
-            locationElement.textContent = `Location: ${location}.`;
+            // Add universal data to the DOM
+            updateUniversalData(location, time);
 
-            const timeElement = document.createElement("p");
-            timeElement.textContent = `Local Time: ${time}.`;
-
-            const day1DateElement = document.createElement("p");
-            day1DateElement.textContent = `Date: ${day1Date}.`;
-
-            const day1MaxTempElement = document.createElement("p");
-            day1MaxTempElement.textContent = `Max: ${day1MaxTemp}°C.`;
-
-
-
-            // Index Through The Divs
-            const locationData = document.getElementById("data");
-            locationData.appendChild(locationElement);
-            locationData.appendChild(timeElement);
-
-            // Append the data to the "day1" div
-            const day1 = document.getElementById("day1");
-            day1.appendChild(day1DateElement);
-            day1.appendChild(day1MaxTempElement);
-            
-            const heading = document.getElementById("heading");
-            heading.innerHTML = `Forecast For ${location}:`;
+            // Extract and display weather data for each day
+            for (let i = 0; i < numberOfDays; i++) {
+                appendData(i, data);
+            }
         })
         .catch(error => {
             console.error("Error fetching weather data:", error);
+            // Display an error message to the user on the webpage
+            showErrorToUser();
         });
 }
 
-// Call the currentAstroData function when the page loads
+function updateUniversalData(location, time) {
+    const locationElement = document.createElement("p");
+    locationElement.textContent = `Location: ${location}.`;
+    const timeElement = document.createElement("p");
+    timeElement.textContent = `Local Time: ${time}.`;
+    const locationData = document.getElementById("data");
+    locationData.appendChild(locationElement);
+    locationData.appendChild(timeElement);
+    const heading = document.getElementById("heading");
+    heading.innerHTML = `Forecast For ${location}:`;
+}
+
+function appendData(index, data) {
+    const dayElement = document.getElementById(`day${index + 1}`);
+    const forecastDay = data.forecast.forecastday[index + 1];
+
+    if (forecastDay) {
+        const date = forecastDay.date;
+        const maxTemp = forecastDay.day.maxtemp_c;
+        const minTemp = forecastDay.day.mintemp_c;
+        const avgTemp = forecastDay.day.avgtemp_c;
+        const windSpeed = forecastDay.day.maxwind_kph;
+        const visibility = forecastDay.day.avgvis_km;
+        const humidity = forecastDay.day.avghumidity;
+
+        const dateElement = document.createElement('p');
+        dateElement.textContent = `Date: ${date}.`;
+        const maxTempElement = document.createElement('p');
+        maxTempElement.textContent = `Max Temperature: ${maxTemp}°C.`;
+        const minTempElement = document.createElement('p');
+        minTempElement.textContent = `Min Temperature: ${minTemp}°C.`;
+        const avgTempElement = document.createElement('p');
+        avgTempElement.textContent = `Average Temperature: ${avgTemp}°C.`;
+        const windSpeedElement = document.createElement('p');
+        windSpeedElement.textContent = `Max Wind Speed: ${windSpeed}kph.`;
+        const visibilityElement = document.createElement('p');
+        visibilityElement.textContent = `Average Visibility: ${visibility}km.`;
+        const humidityElement = document.createElement('p');
+        humidityElement.textContent = `Average Humidity: ${humidity}%.`;
+
+        dayElement.appendChild(dateElement);
+        dayElement.appendChild(maxTempElement);
+        dayElement.appendChild(minTempElement);
+        dayElement.appendChild(avgTempElement);
+        dayElement.appendChild(windSpeedElement);
+        dayElement.appendChild(visibilityElement);
+        dayElement.appendChild(humidityElement);
+    } else {
+        // Handle the case where data for this day is not available
+        const notAvailableElement = document.createElement('p');
+        notAvailableElement.textContent = 'Weather data not available for this day.';
+        dayElement.appendChild(notAvailableElement);
+    }
+}
+
 currentForecast(cityParameter);
